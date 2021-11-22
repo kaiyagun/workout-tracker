dca526880ad0d175e4d33741b02479abcfb7deed
kaiya gunsauls <kaiyagunsauls@gmail.com>
2021-11-21 17:21:11 -0800

    maybe this will work

--- controllers/workoutRoutes.js
+++ controllers/workoutRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const db = require('../models/Workout')
+const db = require('../models/Workout.js')
 
 
 router.get('/',(req,res) => {
